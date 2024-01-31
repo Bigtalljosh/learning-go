257d74f43c90596122b8491abdadb78d787ee798
Josh Dadak <Bigtalljosh@users.noreply.github.com>
2024-01-31 21:46:00 +0000

    static file handlers

--- pluralsight/main.go
+++ pluralsight/main.go
@@ -3,21 +3,55 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
 
-	http.Handle("/", myHandler("Customer service"))
+	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("."))))
+
+	http.HandleFunc("/servecontent", func(w http.ResponseWriter, r *http.Request) {
+		customerFile, err := os.Open("./customers.csv")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer customerFile.Close()
+
+		http.ServeContent(w, r, "customers.csv", time.Now(), customerFile)
+	})
+
+	http.HandleFunc("/servefile", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "customers.csv")
+	})
+
+	http.HandleFunc("/fprint", func(w http.ResponseWriter, r *http.Request) {
+		customerFile, err := os.Open("./customers.csv")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer customerFile.Close()
+
+		data, err := io.ReadAll(customerFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Fprint(w, string(data))
+
+		// io.Copy(w, customerFile)
+
+	})
 
 	s := http.Server{
 		Addr: ":3000",
 	}
 
 	go func() {
-		log.Fatal(s.ListenAndServeTLS("./cert.pem", "./key.pem"))
+		log.Fatal(s.ListenAndServe())
 	}()
 
 	fmt.Println("Server started, press <Enter> to shutdown")
